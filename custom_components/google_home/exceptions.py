cd3ba694909fc4c544805e4ff36d053ee4d500e9
Denys Dovhan <denysdovhan@gmail.com>
2022-02-02 15:38:06 +0200

    Ignore HACS components

--- custom_components/google_home/exceptions.py
+++
@@ -1,6 +0,0 @@
-"""The errors of GoogleHome integration."""
-from homeassistant.exceptions import HomeAssistantError
-
-
-class InvalidMasterToken(HomeAssistantError):
-    """Error to indicate the master token is invalid."""
